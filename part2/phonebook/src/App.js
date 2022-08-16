66615d0a25e1744d39c51a195240a3116b671300
jmc-bbk <jmcneilis@outlook.com>
2022-08-16 20:33:43 +0100

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,50 @@
 import {useState} from 'react'
 
+const Filter = ({filter, handleFilter}) => {
+  return (
+    <div>
+      filter shown with
+      <input value={filter} onChange={handleFilter}/>
+    </div>
+  )
+}
+
+const Persons = ({persons, filter}) => {
+  const filterPersons = () => {
+    return persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  }
+
+  const newPersons = filterPersons()  
+  
+  return (
+    newPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+  )
+}
+
+const PersonForm = ({newName, newNumber, handleNameChange, handleNumberChange, handleAdd}) => {
+  return (
+    <form>
+      <div>
+        name:
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number:
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button onClick={handleAdd} type="submit">add</button>
+      </div>
+    </form>
+  )
+}  
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
@@ -9,13 +54,23 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
+  // It could be best to move these handlers to the child components.  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filterPersons = () => {
+    return persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
   }
 
   const handleAdd = (event) => {
@@ -39,27 +94,23 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name:
-          <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button onClick={handleAdd} type="submit">add</button>
-        </div>
-      </form>
+      <Filter 
+        filter={filter}
+        handleFilter={handleFilter}
+      />
+      <h2>Add new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleAdd={handleAdd}
+      />
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <Persons
+        persons={persons}
+        filter={filter} 
+      />
     </div>
   )
 }
